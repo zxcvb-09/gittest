74cc5124bc359f6c5b3e69c05b05c7eaf5481a16
yuyan <1104557652@qq.com>
2023-04-11 09:39:20 +0800

    Mast 合并冲突

--- src/main/java/gittest/gitTest.java
+++ src/main/java/gittest/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("Mast");
         System.out.println("Mast 2");
         System.out.println("HOt ");
+        System.out.println("合并冲突测试");
     }
 }
